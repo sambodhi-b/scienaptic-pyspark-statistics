160c557e4ecf4467a168aee977165cef35bffc4a
Sambodhi Bhattacharyya <sambodhi.b@gmail.com>
2020-04-14 16:48:39 +0530

    Added script to start Jupyter Notebook server with pyspark

---
+++ start_environment.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Checking if running from inside virtual environment
+#  and activating if not
+[ -z "${VIRTUAL_ENV}" ] && source local_spark_venv/bin/activate
+
+# Setting Spark Home
+export SPARK_HOME="$(python3 -c 'from distutils.sysconfig import get_python_lib; print(get_python_lib())')/pyspark"
+
+# Setting Python to run in Spark Workers
+export PYSPARK_PYTHON='python3'
+
+# Setting PySpark to run in Jupyter Notebook
+export PYSPARK_DRIVER_PYTHON='jupyter'
+export PYSPARK_DRIVER_PYTHON_OPTS='notebook'
+
+pyspark
+
